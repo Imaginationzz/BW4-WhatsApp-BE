6d51f12292d4d4e1d29603b4a31a1c7e53bab650
zenocoz <federico.soncini@gmail.com>
2021-03-01 12:03:16 +0100

    boilerplate first commit

---
+++ src/services/index.js
@@ -0,0 +1,12 @@
+const mainRoute = require("express").Router()
+
+// //SERVICES ROUTES IMPORTS
+// const userRoute = require("./user");
+// const roomRoute = require("../utilities/socket/services/rooms");
+
+//ENDPOINT
+// mainRoute.use("/users", userRoute);
+// mainRoute.use("/socket-rooms", userRoute);
+
+//EXPORT
+module.exports = mainRoute
