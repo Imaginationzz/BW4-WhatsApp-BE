f1c161f46713c11c14ac7c5d6288b2c4632e3b9d
RenieJr <reniejr@gmail.com>
2021-03-01 17:03:27 +0100

    wrap up in socket.join

--- src/socketUtils/index.js
+++ src/socketUtils/index.js
@@ -4,8 +4,8 @@ const {
   getMembersList,
   getMember,
   removeMember,
-} = require("./roomTools")
-const { saveMessage } = require("./chatTools")
+} = require("./roomTools");
+const { saveMessage } = require("./chatTools");
 
 //JOIN ROOM
 const joinRoom = (socket, io) => {
@@ -15,79 +15,79 @@ const joinRoom = (socket, io) => {
       const { username, roomName } = await addMember({
         socketId: socket.id,
         ...data,
-      })
+      });
 
       //SOCKET JOIN TO ROOM
-      socket.join(roomName)
+      socket.join(roomName, async () => {
+        //ALERT MESSAGE WHEN JOIN THE ROOM
+        const joinAlert = {
+          sender: "Admin",
+          text: `${username} has joined the room (${roomName})`,
+          createdAt: new Date(),
+        };
 
-      //ALERT MESSAGE WHEN JOIN THE ROOM
-      const joinAlert = {
-        sender: "Admin",
-        text: `${username} has joined the room (${roomName})`,
-        createdAt: new Date(),
-      }
+        //SEND THE ALERT TO THE ROOM
+        socket.broadcast.to(roomName).emit("message", joinAlert);
 
-      //SEND THE ALERT TO THE ROOM
-      socket.broadcast.to(roomName).emit("message", joinAlert)
+        //MEMBERS LIST
+        const membersList = await getMembersList(roomName);
 
-      //MEMBERS LIST
-      const membersList = await getMembersList(roomName)
-
-      //SEND MEMBERS LIST
-      io.to(roomName).emit("membersList", { roomName, list: membersList })
+        //SEND MEMBERS LIST
+        io.to(roomName).emit("membersList", { roomName, list: membersList });
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  })
-}
+  });
+};
 
 //CHATGROUP
 const chat = (socket, io) => {
   return socket.on("chat", async ({ roomName, message }) => {
     //FIND USER
-    const member = await getMember(roomName, socket.id)
+    const member = await getMember(roomName, socket.id);
 
     //MESSAGE
     const messageContent = {
       text: message,
       sender: member.username,
       roomName,
-    }
+    };
 
     //SAVE MESSAGE IN DB
-    await saveMessage(messageContent)
+    await saveMessage(messageContent);
 
     //SEND MeSSAGE TO CHAT
-    io.to(roomName).emit("message", messageContent)
-  })
-}
+    io.to(roomName).emit("message", messageContent);
+  });
+};
 
 //LEAVE ROOM
 const leaveRoom = (socket, io) => {
   return socket.on("leaveRoom", async ({ roomName }) => {
     try {
-      const member = await removeMember(roomName, socket.id)
+      const member = await removeMember(roomName, socket.id);
 
       //LEAVE ALERT
       const leaveAlert = {
         sender: "Admin",
         text: `${member.username} has left the room (${roomName})`,
         createdAt: new Date(),
-      }
+      };
 
       //SEND LEAVE ALERT
-      io.to(roomName).emit("message", leaveAlert)
+      io.to(roomName).emit("message", leaveAlert);
 
       //UPDATE MEMBERS LIST
-      const membersList = await getMembersList(roomName)
+      const membersList = await getMembersList(roomName);
 
       //SEND UPDATED MEMBERS LIST
-      io.to(roomName).emit("membersList", { roomName, list: membersList })
+      io.to(roomName).emit("membersList", { roomName, list: membersList });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  })
-}
+  });
+};
 
 //EXPORTS
-module.exports = { joinRoom, chat, leaveRoom }
+module.exports = { joinRoom, chat, leaveRoom };
